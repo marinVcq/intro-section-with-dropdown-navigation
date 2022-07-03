f9261b745750f1f5a6792a2b504274cc845224fd
Marinos <76847464+marinVcq@users.noreply.github.com>
2022-07-03 19:27:06 +0200

    dropdown item script

---
+++ script.js
@@ -0,0 +1,23 @@
+var dropdownButton = document.getElementsByClassName('dropdown-btn');
+var dropContent = document.getElementsByClassName('dropdown-content');
+var arrow = document.getElementsByClassName('icon-arrow');
+
+for(let i = 0; i<dropdownButton.length; i++){
+  dropdownButton[i].addEventListener('click', function(){
+    let content = dropContent[i];
+    let arr = arrow[i];
+
+    if(content.classList.contains('visible')){
+      content.classList.remove('visible');
+      arr.classList.remove('up');
+      arr.classList.add('down');
+
+    }else{
+      content.classList.add('visible');
+      arr.classList.remove('down');
+      arr.classList.add('up');
+
+
+    }
+  });
+}
