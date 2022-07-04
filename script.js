b2869c39b408a30b230ff7d2b1d825d07c68915c
Marinos <76847464+marinVcq@users.noreply.github.com>
2022-07-04 16:46:36 +0200

    SASS structure + minor CSS fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var dropdownButton = document.getElementsByClassName('dropdown-btn');
-var dropContent = document.getElementsByClassName('dropdown-content');
+var dropContent = document.getElementsByClassName('dropdown-box');
 var arrow = document.getElementsByClassName('icon-arrow');
 
 for(let i = 0; i<dropdownButton.length; i++){
   dropdownButton[i].addEventListener('click', function(){
+
+    let btn = dropdownButton[i];
     let content = dropContent[i];
     let arr = arrow[i];
 
@@ -11,11 +13,14 @@ for(let i = 0; i<dropdownButton.length; i++){
       content.classList.remove('visible');
       arr.classList.remove('up');
       arr.classList.add('down');
+      btn.classList.remove('selected');
 
     }else{
       content.classList.add('visible');
       arr.classList.remove('down');
       arr.classList.add('up');
+      btn.classList.add('selected');
+
     }
   });
 }
